ee1941998eeca816f0483b579bb37407cfe23889
pawel.wilgan <pawel.wilgan@openprojects.com.pl>
2021-06-07 16:04:35 +0200

    Init

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import logo from '../../assets/DeSmart-logo-black-500px.png'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <header className='app-header'>
+        <img src={logo} className='app-logo' alt='logo' />
+        <p>Please select one item:</p>
+      </header>
+      <div>
+        {/* render list here */}
+      </div>
+    </div>
+  )
+}
+
+export default App
